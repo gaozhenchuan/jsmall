b9d7b2f60601cc3320eeda0b982a2bc251e63a14
2632-800-FBSE16\2632-800-FBSE16 <gaozhencuan@gmail.com>
2018-02-09 17:43:56 +0900

    Hello World !!!

---
+++ jsmall-dao/src/main/java/org/jsmall/dao/App.java
@@ -0,0 +1,13 @@
+package org.jsmall.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jsmall-service/src/main/java/org/jsmall/service/App.java
@@ -0,0 +1,13 @@
+package org.jsmall.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
